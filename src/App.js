70e6706436d4645bf202e497e1657b0a892efc01
Thonk <75491816+TechnoShip123@users.noreply.github.com>
2022-05-09 06:39:12 -0500

    :sparkles: :test_tube: Created barebones app & added tests

--- src/App.js
+++ src/App.js
@@ -1,12 +1,105 @@
-import logo from "./logo.svg";
+import React, { useState } from "react";
+import axios from "axios";
+// TODO: Use unique favicon
+import pokeballIcon from "./pokeball_icon.svg";
 import "./App.css";
 
 function App() {
+  /** Marks if the data is currently being retrieved. */
+  const [isLoading, setIsLoading] = useState(true);
+  /** Intended for the first run, tracks if a query has been submitted before.
+   * Used to determine if 'tutorial' message should be shown. */
+  const [isQueryAvailable, setIsQueryAvailable] = useState(false);
+  /** Marks the Pokemon being requested. */
+  const [pokemon, setPokemon] = useState("pikachu");
+  /** Marks the Pokemon query input from the user (before submit). */
+  const [pokemonQuery, setPokemonQuery] = useState("");
+  /** Holds the API data for the pokemon. */
+  const [pokemonData, setPokemonData] = useState([]);
+  /** Holds the Pokemon's energy type for easy access, taken from API data. */
+  const [pokemonType, setPokemonType] = useState("");
+
+  /** Modifies the {@link pokemonQuery} to reflect changes in user input. */
+  const handleSearchBoxChange = (e) => {
+    setPokemonQuery(e.target.value.toLowerCase());
+  };
+
+  /** Handles tasks when the user submits a Pokemon query. */
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    setIsQueryAvailable(true);
+    setIsLoading(true);
+    getPokemonData();
+  };
+
+  /** Retrieves the Pokemon's API data using the query given by the user. */
+  const getPokemonData = async () => {
+    /* Using pokemonQuery here so that the previous valid data is shown
+    if an error occurs while querying new data from the API */
+    const ENDPOINT = "https://pokeapi.co/api/v2/pokemon/" + pokemonQuery;
+    const pokemonDataArray = [];
+    axios(ENDPOINT)
+      .then((res) => {
+        setIsLoading(false);
+        console.info("POKEMON API RESPONSE:\n", res);
+
+        if (res.data) {
+          pokemonDataArray.push(res.data);
+          setPokemonType(res.data.types[0].type.name);
+          setPokemon(res.data.forms[0].name);
+          console.warn(res.data.types[0].name);
+          setPokemonData(pokemonDataArray);
+        } else {
+          console.error("Pokemon data not found!");
+        }
+      })
+      .catch((err) => {
+        setIsLoading(false);
+        // TODO: Display error in HTML instead of alerts.
+        alert(
+          "An error occured while retrieving Pokemon data:\n" +
+            err.code +
+            "\nCheck your spelling, and ensure you are connected to the internet."
+        );
+        console.error("An error occured while retrieving Pokemon data:\n", err);
+      });
+  };
+
+  // TODO: Animated loading? e.g. dots bounce up
+  const pokemonAPIContent = !isQueryAvailable ? (
+    <div>Enter a Pokemon to see its data!</div>
+  ) : isLoading ? (
+    <div>Fetching data...</div>
+  ) : (
+    <div>
+      {/* TODO: Convert to neat table, and add more data like different forms. */}
+      {pokemonData.map((data) => {
+        return (
+          <div>
+            <h5 style={{ textTransform: "capitalize" }}>{pokemon}</h5>
+            Type : <code>{pokemonType}</code>
+          </div>
+        );
+      })}
+    </div>
+  );
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello, world!</p>
+        <img src={pokeballIcon} className="App-logo" alt="logo" />
+        <p>Welcome to PokédexJS!</p>
+        <form onSubmit={handleFormSubmit}>
+          <label>
+            <input
+              type="text"
+              role={"pokemon-query-box"}
+              onChange={handleSearchBoxChange}
+              placeholder="Pokemon name"
+            />
+          </label>
+        </form>
+        {pokemonAPIContent}
       </header>
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("Renders Pokemon search box", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const queryBox = screen.getByRole("pokemon-query-box");
+  expect(queryBox).toBeInTheDocument();
 });
